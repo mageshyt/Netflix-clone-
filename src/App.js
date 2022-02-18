0b88816df1b759139a5f317214782f64a41fe88e
magesh <hariarunengg@gmail.com>
2022-02-18 10:32:32 +0530

    removed unused code 🗑️

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/info" element={<MovieInfoPage />}></Route>
       </Routes>
-      <Footer />
     </div>
   );
 }
